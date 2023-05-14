9bf971a334e181e2e6cf6296ed801768b1cf6d73
daimessdn@gmail.com <daimessdn@gmail.com>
2023-05-14 20:01:12 +0700

    added font features

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useCallback } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
+
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 import Button from './src/components/Button';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Inter-Bold': require('./assets/fonts/Inter-Bold.ttf'),
+    'Inter-Regular': require('./assets/fonts/Inter-Regular.ttf'),
+    'Inter-Light': require('./assets/fonts/Inter-Light.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Selamat datang di Aplikasi C3</Text>
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={{ fontFamily: "Inter-Regular", marginBottom: 12 }}>Selamat datang di Aplikasi C3</Text>
       <Button type="primary" text="Login" onPress={() => console.log("login disini")} />
 
       <StatusBar style="auto" />
